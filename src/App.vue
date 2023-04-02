46effb2eb5af2381f6e7002e89ad860b97d2b75d
Pavel Ryaposov <pavel.ryaposov@booking.com>
2023-04-02 17:31:13 +0200

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <ion-app>
+    <ion-router-outlet ref="presentingElement" />
+    <ion-modal
+      ref="modal"
+      :is-open="true"
+      :presenting-element="presentingElement.$el"
+      can-dismiss
+      @didDismiss="emit('dismiss', false)"
+    >
+      <ion-nav
+        id="order-modal-nav"
+        ref="navRef"
+        :root="OrderModalHome"
+      />
+    </ion-modal>
+  </ion-app>
+</template>
+
+<script setup lang="ts">
+import { IonApp, IonRouterOutlet, IonModal, IonNav } from '@ionic/vue';
+</script>
